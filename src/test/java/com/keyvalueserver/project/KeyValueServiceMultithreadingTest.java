02be627975335e380a7da146649584ed000ba552
julianzhan <julianzhan6@gmail.com>
2024-01-04 18:06:07 +0800

    separate runnable request

--- src/test/java/com/keyvalueserver/project/KeyValueServiceMultithreadingTest.java
+++ src/test/java/com/keyvalueserver/project/KeyValueServiceMultithreadingTest.java
@@ -5,32 +5,56 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-
-
-class RunnableRequest implements Runnable {
-
+class SetRequest implements Runnable {
     private KeyValueService keyValueService;
-    private String operation;
     private String key;
     private String value;
 
-    protected RunnableRequest(KeyValueService keyValueService, String operation, String key, String value) {
+    protected SetRequest(KeyValueService keyValueService, String key, String value) {
         this.keyValueService = keyValueService;
-        this.operation = operation;
         this.key = key;
         this.value = value;
     }
 
     public void run() {
-        if (operation.equals("set")) {
-            keyValueService.setKeyValue(key, value);
-        } else if (operation.equals("get")) {
-            keyValueService.getKeyValue(key);
-        } else if (operation.equals("delete")) {
-            keyValueService.deleteKeyValue(key);
-        }
+        keyValueService.setKeyValue(key, value);
     }
 }
+
+class GetRequest implements Runnable {
+    private KeyValueService keyValueService;
+    private String key;
+    private String value;
+
+    protected GetRequest(KeyValueService keyValueService, String key) {
+        this.keyValueService = keyValueService;
+        this.key = key;
+    }
+
+    public void run() {
+        value = keyValueService.getKeyValue(key);
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
+class DeleteRequest implements Runnable {
+    private KeyValueService keyValueService;
+    private String key;
+
+    protected DeleteRequest(KeyValueService keyValueService, String key) {
+        this.keyValueService = keyValueService;
+        this.key = key;
+    }
+
+    public void run() {
+        keyValueService.deleteKeyValue(key);
+    }
+}
+
+
 
 public class KeyValueServiceMultithreadingTest {
 
@@ -45,10 +69,11 @@ public class KeyValueServiceMultithreadingTest {
     void testKeyValueMultithreading() {
         final int numThreads = 10000;
         Thread[] threads = new Thread[numThreads];
+        GetRequest[] getRequest = new GetRequest[numThreads];
 
         for (int i = 0; i < numThreads; i++) {
-            RunnableRequest runnableRequest = new RunnableRequest(keyValueService, "set", "testKey" + i, "testValue" + i);
-            threads[i] = new Thread(runnableRequest);
+            SetRequest setRequest = new SetRequest(keyValueService, "testKey" + i, "testValue" + i);
+            threads[i] = new Thread(setRequest);
             threads[i].start();
         }
         for (int i = 0; i < numThreads; i++) {
@@ -59,12 +84,21 @@ public class KeyValueServiceMultithreadingTest {
             }
         }
         for (int i = 0; i < numThreads; i++) {
-            String value = keyValueService.getKeyValue("testKey" + i);
-            assertEquals(value, "testValue" + i);
+            getRequest[i] = new GetRequest(keyValueService, "testKey" + i);
+            threads[i] = new Thread(getRequest[i]);
+            threads[i].start();
         }
         for (int i = 0; i < numThreads; i++) {
-            RunnableRequest runnableRequest = new RunnableRequest(keyValueService, "delete", "testKey" + i, "testValue" + i);
-            threads[i] = new Thread(runnableRequest);
+            try {
+                threads[i].join();
+                assertEquals("testValue" + i, getRequest[i].getValue());
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
+        for (int i = 0; i < numThreads; i++) {
+            DeleteRequest deleteRequest = new DeleteRequest(keyValueService, "testKey" + i);
+            threads[i] = new Thread(deleteRequest);
             threads[i].start();
         }
         for (int i = 0; i < numThreads; i++) {
@@ -75,8 +109,17 @@ public class KeyValueServiceMultithreadingTest {
             }
         }
         for (int i = 0; i < numThreads; i++) {
-            String value = keyValueService.getKeyValue("testKey" + i);
-            assertNull(value);
+            getRequest[i] = new GetRequest(keyValueService, "testKey" + i);
+            threads[i] = new Thread(getRequest[i]);
+            threads[i].start();
+        }
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join();
+                assertNull(getRequest[i].getValue());
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
         }
     }
 
