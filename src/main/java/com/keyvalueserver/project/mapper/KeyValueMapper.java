bbf2ee80891d58b5031646a8938e4912610406dc
julianzhan <julianzhan6@gmail.com>
2024-01-11 10:29:09 +0800

    set up mybatis mapper with spring boot app through model and repository

--- src/main/java/com/keyvalueserver/project/mapper/KeyValueMapper.java
+++ src/main/java/com/keyvalueserver/project/mapper/KeyValueMapper.java
@@ -1,11 +1,10 @@
-//package com.keyvalueserver.project.mapper;
-//
-//import com.keyvalueserver.project.model.KeyValueModel;
-//import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Select;
-//
-//@Mapper
-//public interface KeyValueMapper {
-//    @Select("SELECT * FROM keyvaluepair WHERE key = #{key}")
-//    KeyValueModel getValueByKey(String key);
-//}
+package com.keyvalueserver.project.mapper;
+
+import com.keyvalueserver.project.model.KeyValuePair;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KeyValueMapper {
+    void insertOrUpdateKeyValue(KeyValuePair keyValue);
+    void deleteKeyValue(String key);
+}
--- src/main/java/com/keyvalueserver/project/repository/KeyValueRepository.java
+++ src/main/java/com/keyvalueserver/project/repository/KeyValueRepository.java
@@ -1,4 +1,24 @@
 package com.keyvalueserver.project.repository;
 
+import com.keyvalueserver.project.mapper.KeyValueMapper;
+import com.keyvalueserver.project.model.KeyValuePair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class KeyValueRepository {
+    private final KeyValueMapper keyValueMapper;
+
+    @Autowired
+    public KeyValueRepository(KeyValueMapper keyValueMapper) {
+        this.keyValueMapper = keyValueMapper;
+    }
+
+    public void insertOrUpdateKeyValue(KeyValuePair keyValue) {
+        keyValueMapper.insertOrUpdateKeyValue(keyValue);
+    }
+
+    public void deleteKeyValue(String key) {
+        keyValueMapper.deleteKeyValue(key);
+    }
 }
